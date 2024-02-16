2366b04c3da07df39a77ee510addd1b173a82573
Asjad  Iftikhar <asjad.iftikhar@crowdbotics.com>
2024-02-16 13:51:29 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "modulestestapp-45658",
-  url: "https://modulestestapp-45658.botics.co",
-  api: "https://modulestestapp-45658.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "modulestestapp-45658",
+  "url": "https://modulestestapp-45658.botics.co",
+  "api": "https://modulestestapp-45658.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "modules"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
